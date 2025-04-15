cd44db91e0d546c0f4910efab58b108ff8bd355c
Jitendra Singh <jeet4v25@gmail.com>
2025-04-15 14:54:57 +0530

    Add project files.

---
+++ Repository/CategoryRepository.cs
@@ -0,0 +1,61 @@
+﻿using BlazorYum.Data;
+using BlazorYum.Repository.IRepository;
+
+namespace BlazorYum.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public Category Create(Category obj)
+        {
+            _db.Category.Add(obj);
+            _db.SaveChanges();
+            return obj;
+        }
+
+        public bool Delete(int id)
+        {
+            var item = _db.Category.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                _db.Category.Remove(item);
+                return _db.SaveChanges() > 0;
+            }
+            return false;
+        }
+
+        public Category Get(int id)
+        {
+            var item = _db.Category.FirstOrDefault(x => x.Id == id);
+            if (item == null)
+            {
+                item = new Category();
+            }
+
+            return item;
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            return _db.Category.ToList();
+        }
+
+        public Category Update(Category obj)
+        {
+            var item = _db.Category.FirstOrDefault(x => x.Id == obj.Id);
+            if (item != null)
+            {
+                item.Name = obj.Name;
+                _db.Category.Update(item);
+                _db.SaveChanges();
+                return item;
+            }
+
+            return obj;
+        }
+    }
+}
